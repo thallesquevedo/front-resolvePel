e132be6deb9752c1e72f3088f7e0c8740c808db6
Pamela Silva Woigt <paswoigt@gmail.com>
2024-06-23 17:44:48 -0300

    adicionando testes na classe cadastro.test.tsx para verificação das mensagem de erro ao não preencher campos obrigatorios

--- app/prestador/cadastro.test.tsx
+++ app/prestador/cadastro.test.tsx
@@ -2,8 +2,10 @@ import { TamaguiProvider } from 'tamagui';
 
 import Cadastro from './cadastro';
 import config from '~/tamagui.config';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { render, waitFor, fireEvent } from '@testing-library/react-native';
 import { useAuth } from '~/context/auth-context';
+import { number } from 'yup';
+import { router } from 'expo-router';
 
 describe('Cadastro', () => {
     const mockCadastro = (
@@ -42,8 +44,6 @@ describe('Cadastro', () => {
     //     expect(router.navigate).toHaveBeenCalledWith('./login');
     // });
 
-    //criar cenário com textos de campos obrigatórios
-    // --------------nome, cpf, email, telefone, senha, repetir senha
     //criar cenário que valida cadastro com sucesso
     //criar cenário cpf menor que 11 caracteres e sua respectiva mensagem
     //criar cenário com email inválido e sua respectiva mensagem
@@ -52,19 +52,98 @@ describe('Cadastro', () => {
     //criar cenário de senha e repetir senha diferentes e sua respectiva mensagem
     //criar cenário que cadastra e vai apra login
 
-    //esse teste que não reconhece a mensagem de erro do yup
-          //existe um nos mesmo moldes no inde.test.tsx
-    it('must show mandatory field message' , async () => {
+    it('must show a mandatory field message when trying to finish without filling in the name field' , async () => {
   
-        const { getByPlaceholderText, findByText } = render(mockCadastro);
-      
-        const campoNome = getByPlaceholderText('Ex: João das Neves');
-        const campoCPF = getByPlaceholderText('Ex: 000.000.000-00');
-        
-        fireEvent.changeText(campoNome, ' ');
-        fireEvent.press(campoCPF);
+        const {getByPlaceholderText, getByText, queryByText} = render(mockCadastro);
 
-        const mensagemErro = await findByText('Nome é obrigatório');
-        expect(mensagemErro).toBeTruthy();
+        const campoNome = getByPlaceholderText('Ex: João das Neves');
+        const buttonFinalizaCadastro = getByText('Finalizar cadastro');
+
+        expect(queryByText('Nome é obrigatório')).toBeNull();
+
+        fireEvent.press(campoNome);
+        fireEvent.press(buttonFinalizaCadastro);
+
+        await waitFor(() => {
+          expect(getByText('Nome é obrigatório')).toBeTruthy();
+        });
+
+        expect(getByText('Nome é obrigatório')).not.toBeNull();
     });
+
+    it('must show a mandatory field message when trying to finish without filling in the CPF field' , async () => {
+  
+      const {getByPlaceholderText, getByText, queryByText} = render(mockCadastro);
+
+      const campoCPF = getByPlaceholderText('Ex: 000.000.000-00');
+      const buttonFinalizaCadastro = getByText('Finalizar cadastro');
+
+      expect(queryByText('CPF é obrigatório')).toBeNull();
+
+      fireEvent.press(campoCPF);
+      fireEvent.press(buttonFinalizaCadastro);
+
+      await waitFor(() => {
+        expect(getByText('CPF é obrigatório')).toBeTruthy();
+      });
+
+      expect(getByText('CPF é obrigatório')).not.toBeNull();
+  });
+
+  it('must show a mandatory field message when trying to finish without filling in the phone number field' , async () => {
+  
+    const {getByPlaceholderText, getByText, queryByText} = render(mockCadastro);
+
+    const campoPhone = getByPlaceholderText('Ex: (DDD) 9 9999.9999');
+    const buttonFinalizaCadastro = getByText('Finalizar cadastro');
+
+    expect(queryByText('Telefone é obrigatório')).toBeNull();
+
+    fireEvent.press(campoPhone);
+    fireEvent.press(buttonFinalizaCadastro);
+
+    await waitFor(() => {
+      expect(getByText('Telefone é obrigatório')).toBeTruthy();
+    });
+
+    expect(getByText('Telefone é obrigatório')).not.toBeNull();
+  });
+
+  it('must show a mandatory field message when trying to finish without filling in the senha field' , async () => {
+  
+    const {getByPlaceholderText, getByText, queryByText} = render(mockCadastro);
+
+    const campoSenha = getByPlaceholderText('Informe sua senha');
+    const buttonFinalizaCadastro = getByText('Finalizar cadastro');
+
+    expect(queryByText('Senha é obrigatório')).toBeNull();
+
+    fireEvent.press(campoSenha);
+    fireEvent.press(buttonFinalizaCadastro);
+
+    await waitFor(() => {
+      expect(getByText('Senha é obrigatório')).toBeTruthy();
+    });
+
+    expect(getByText('Senha é obrigatório')).not.toBeNull();
+  });
+
+  it('must show a mandatory field message when trying to finish without filling in the repeat senha field' , async () => {
+  
+    const {getByPlaceholderText, getByText, queryByText} = render(mockCadastro);
+
+    const campoRepeatSenha = getByPlaceholderText('Repita sua senha');
+    const buttonFinalizaCadastro = getByText('Finalizar cadastro');
+
+    expect(queryByText('Campo obrigatório')).toBeNull();
+
+    fireEvent.press(campoRepeatSenha);
+    fireEvent.press(buttonFinalizaCadastro);
+
+    await waitFor(() => {
+      expect(getByText('Campo obrigatório')).toBeTruthy();
+    });
+
+    expect(getByText('Campo obrigatório')).not.toBeNull();
+  });
 });
